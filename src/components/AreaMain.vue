3c7b27482b1ec964ffecb13e73c56365e224a0bb
supplepentan <pentagramog@gmail.com>
2022-06-13 22:15:28 +0900

    first commit

---
+++ src/components/AreaMain.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue';
+import TextCount from './parts/TextCount.vue';
+
+const count = ref(0)
+</script>
+
+<template>
+    <h1>メイン</h1>
+    <TextCount />
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/parts/TextCount.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue';
+const words = ref("🐰")
+const addRabbit = () => {
+    console.log("🐰")
+    words.value += "🐰"
+}
+const calearRabbit = () => {
+    words.value = ""
+}
+</script>
+
+<template>
+    <button @click="addRabbit">🐰+</button>
+    <button @click="calearRabbit">🐰-</button>
+    <div>{{ words }}</div>
+</template>
