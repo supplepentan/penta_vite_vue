7db8ef57d1f518748cd5c427ce7adc3ed4e01864
supplepentan <pentagramog@gmail.com>
2022-06-14 20:19:12 +0900

    add tailwind

--- src/components/AreaMain.vue
+++ src/components/AreaMain.vue
@@ -3,7 +3,9 @@ import ImageSvg from './parts/ImageSvg.vue';
 </script>
 
 <template>
-    <h1>メイン</h1>
+    <h1 class="text-3xl font-bold underline">
+        Hello world!
+    </h1>
     <ImageSvg />
 </template>
 
