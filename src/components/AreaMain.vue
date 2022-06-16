c08271ec6db43ece8e24758224d4d4d563b7e46d
supplepentan <pentagramog@gmail.com>
2022-06-17 07:11:05 +0900

    update

--- src/components/AreaMain.vue
+++ src/components/AreaMain.vue
@@ -1,10 +1,12 @@
 <script setup>
 import CanvasImageComposition from './parts/CanvasImageComposition.vue';
+import WebCamera from './parts/WebCamera.vue';
 </script>
 
 <template>
     <div>
         <CanvasImageComposition />
+        <WebCamera />
     </div>
 </template>
 
