48f7d9649aa519b87ec9cb1e757e8b2076b81bac
supplepentan <pentagramog@gmail.com>
2022-09-04 16:55:12 +0900

    add: Polygon variable

--- src/components/parts/Fabric/FabricBase.vue
+++ src/components/parts/Fabric/FabricBase.vue
@@ -1,4 +1,5 @@
 <script setup>
+//http://fabricjs.com/
 //https://qiita.com/kurukuruz/items/04866272867f4ace3a17
 //https://qiita.com/kurukuruz/items/966f0c6942df73f35f46
 //https://erabikata.info/javascript-canvas-fabric-image.html
@@ -74,7 +75,7 @@ const getImage = () => {
     link.href = canvasImage.toDataURL("image/png");
     link.download = 'export_image.png';
     link.click();
-  }
+  };
   ctxImage.clearRect(0, 0, canvasImage.width, canvasImage.height);
 };
 // 特定要素の削除
@@ -98,9 +99,10 @@ const elmDelete = () => {
     alert("オブジェクトが選択されていません。");
   }
 };
-const postText = () => {
-  const element = new fabric.Textbox("テキスト", {
-    left: (canvasFab.width / 2),        //左
+//テキスト追加
+const drawText = () => {
+  const element = new fabric.Textbox("Text", {
+    left: (canvasFab.width / 2),
     top: (canvasFab.height / 2),
     originX: "center",
     originY: "center",
@@ -111,16 +113,138 @@ const postText = () => {
   element.center();
   canvasFab.add(element);
 }
+//ポリゴン追加
+const drawPlygon = () => {
+	var points = [{
+		x: 0, y: 4
+	}, {
+		x: 16, y: 0
+	}, {
+		x: 32, y: 4
+	},  {
+		x: 32, y: 32
+	}, {
+		x: 16, y: 36
+	}, {
+		x: 0, y: 32
+	}]
+	var polygon = new fabric.Polygon(points, {
+    left: (canvasFab.width / 2),
+    top: (canvasFab.height / 2),
+    originX: "center",
+    originY: "center",
+		fill: false,
+		strokeWidth: 2,
+    stroke: 'blue',
+		scaleX: 4,
+		scaleY: 4,
+		objectCaching: false,
+		transparentCorners: false,
+		cornerColor: 'blue',
+	});
+  //canvasFab.viewportTransform = [0.7, 0, 0, 0.7, -50, 50];
+	canvasFab.add(polygon);
+};
+// define a function that can locate the controls.
+// this function will be used both for drawing and for interaction.
+function polygonPositionHandler(dim, finalMatrix, fabricObject) {
+  var x = (fabricObject.points[this.pointIndex].x - fabricObject.pathOffset.x),
+	    y = (fabricObject.points[this.pointIndex].y - fabricObject.pathOffset.y);
+	return fabric.util.transformPoint(
+		{ x: x, y: y },
+     fabric.util.multiplyTransformMatrices(
+       fabricObject.canvas.viewportTransform,
+       fabricObject.calcTransformMatrix()
+     )
+	);
+};
+function getObjectSizeWithStroke(object) {
+	var stroke = new fabric.Point(
+		object.strokeUniform ? 1 / object.scaleX : 1, 
+		object.strokeUniform ? 1 / object.scaleY : 1
+	).multiply(object.strokeWidth);
+	return new fabric.Point(object.width + stroke.x, object.height + stroke.y);
+};
+// define a function that will define what the control does
+// this function will be called on every mouse move after a control has been
+// clicked and is being dragged.
+// The function receive as argument the mouse event, the current trasnform object
+// and the current position in canvas coordinate
+// transform.target is a reference to the current object being transformed,
+function actionHandler(eventData, transform, x, y) {
+	var polygon = transform.target,
+	    currentControl = polygon.controls[polygon.__corner],
+	    mouseLocalPosition = polygon.toLocalPoint(new fabric.Point(x, y), 'center', 'center'),
+       polygonBaseSize = getObjectSizeWithStroke(polygon),
+			size = polygon._getTransformedDimensions(0, 0),
+			finalPointPosition = {
+				x: mouseLocalPosition.x * polygonBaseSize.x / size.x + polygon.pathOffset.x,
+				y: mouseLocalPosition.y * polygonBaseSize.y / size.y + polygon.pathOffset.y
+			};
+	polygon.points[currentControl.pointIndex] = finalPointPosition;
+	return true;
+};
+// define a function that can keep the polygon in the same position when we change its
+// width/height/top/left.
+function anchorWrapper(anchorIndex, fn) {
+  return function(eventData, transform, x, y) {
+    var fabricObject = transform.target,
+        absolutePoint = fabric.util.transformPoint({
+            x: (fabricObject.points[anchorIndex].x - fabricObject.pathOffset.x),
+            y: (fabricObject.points[anchorIndex].y - fabricObject.pathOffset.y),
+        }, fabricObject.calcTransformMatrix()),
+        actionPerformed = fn(eventData, transform, x, y),
+        newDim = fabricObject._setPositionDimensions({}),
+        polygonBaseSize = getObjectSizeWithStroke(fabricObject),
+        newX = (fabricObject.points[anchorIndex].x - fabricObject.pathOffset.x) / polygonBaseSize.x,
+		    newY = (fabricObject.points[anchorIndex].y - fabricObject.pathOffset.y) / polygonBaseSize.y;
+    fabricObject.setPositionByOrigin(absolutePoint, newX + 0.5, newY + 0.5);
+    return actionPerformed;
+  }
+};
+function drawPlygonEdit() {
+	// clone what are you copying since you
+	// may want copy and paste on different moment.
+	// and you do not want the changes happened
+	// later to reflect on the copy.
+	var poly = canvasFab.getObjects()[0];
+	canvasFab.setActiveObject(poly);
+	poly.edit = !poly.edit;
+	if (poly.edit) {
+     var lastControl = poly.points.length - 1;
+     poly.cornerStyle = 'circle';
+     poly.cornerColor = 'rgba(0,0,255,0.5)';
+    poly.controls = poly.points.reduce(function(acc, point, index) {
+			acc['p' + index] = new fabric.Control({
+				positionHandler: polygonPositionHandler,
+				actionHandler: anchorWrapper(index > 0 ? index - 1 : lastControl, actionHandler),
+				actionName: 'modifyPolygon',
+				pointIndex: index
+			});
+			return acc;
+		}, { });
+	} else {
+     poly.cornerColor = 'blue';
+     poly.cornerStyle = 'rect';
+		poly.controls = fabric.Object.prototype.controls;
+	}
+	poly.hasBorders = !poly.edit;
+	canvasFab.requestRenderAll();
+};
+
 </script>
 <template>
   <div class="m-2">
-    <h1 class="flex-none text-center">Fabric JS</h1>
-    <div class="grid grid-cols-2">
-      <div class="flex justify-center w-full m-2">
-        <canvas id="canvasFab" width="512" height="512" class="block border-2 bg-gray400"></canvas>
-        <canvas id="canvasImage" width="512" height="512" class="invisible"></canvas>
+    <h1 class="text-center">Fabric JS</h1>
+    <div class="grid grid-flow-col">
+      <div class="relative flex justify-center bg-sky-100">
+        <canvas id="canvasFab" width="512" height="512" class="absolute border-2 bg-gray400 "></canvas>
+        <canvas id="canvasImage" width="512" height="512" class="absolute invisible"></canvas>
       </div>
-      <div class="grid grid-cols-1">
+      <div class="bg-gray-100 ">
+        <p>Layer</p>
+      </div>
+      <div class="bg-gray-300 ">
         <div class="border">
           <label for="loadFile" class="">Uploading Image</label><br />
           <input type="file" id="loadFile" v-on:change="loadFile" required minlength="4" maxlength="8" size="10">
@@ -130,7 +254,9 @@ const postText = () => {
         </div>
         <div class="border">
           <p>Add Object</p>
-          <button type="button" v-on:click="postText" class="p-2 border-2">DrawText</button>
+          <button type="button" v-on:click="drawText" class="p-2 border-2">Text</button>
+          <button type="button" v-on:click="drawPlygon" class="p-2 border-2">Polygon</button>
+          <button type="button" v-on:click="drawPlygonEdit" class="p-2 border-2">Polygon Edit</button>
         </div>
         <div class="border">
           <p>Download</p>
